2b0b0a738afae233034e9966f7a8dae31545b1c0
jhonatan <j.azevedo@ctrlplay.com.br>
2023-07-02 13:21:21 -0300

    savin make-quiz desktop design

--- frontend/src/pages/MakeQuiz.tsx
+++ frontend/src/pages/MakeQuiz.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import QuizQuestionModel from "../models/QuizQuestionModel";
 import QuizQuestionAlternative from "../models/QuizQuestionAlternative";
-import { BsFillTrash3Fill, BsPlusCircleFill } from "react-icons/bs";
+import { BsFillTrash3Fill, BsFillPlusSquareFill } from "react-icons/bs";
 
 interface MakeQuizProps {
   isMenuOpen: boolean;
@@ -121,47 +121,50 @@ export default function MakeQuiz(props: MakeQuizProps) {
   }
 
   return (
-    <div className="flex flex-col gap-y-4 text-white text-center">
-      <p className="text-2xl font-bold">Faça o seu quiz</p>
-      <div className="flex flex-col gap-2">
-        <label className="text-lg font-bold ">Título</label>
+    <div className="flex flex-col gap-y-4 md:gap-y-8 text-white text-center">
+      <p className="text-2xl md:text-6xl font-bold">Faça o seu quiz</p>
+      <div className="flex flex-col gap-2 md:gap-6 md:w-1/2 md:m-auto">
+        <label className="text-lg md:text-4xl font-bold ">Título</label>
         <input
-          className="p-2 bg-gray-950 border-4 rounded-md font-bold border-yellow-400"
+          className="text-lg md:text-4xl p-2 md:p-6 bg-gray-950 border-4 md:border-8 rounded-md font-bold border-yellow-400"
           type="text"
         />
       </div>
-      <div className="flex flex-col gap-2">
-        <label className="text-lg font-bold ">Descrição</label>
+      <div className="flex flex-col gap-2 md:gap-6 md:w-1/2 md:m-auto">
+        <label className="text-lg md:text-4xl font-bold ">Descrição</label>
         <input
-          className="p-2 bg-gray-950 border-4 rounded-md font-bold border-yellow-400"
+          className="text-lg md:text-4xl p-2 md:p-6 bg-gray-950 border-4 md:border-8 rounded-md font-bold border-yellow-400"
           type="text"
         />
       </div>
 
-      <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-16 md:w-2/3 mt-6 md:m-auto md:mt-10">
         {quizQuestions.map((quizQuestion, quizQuestionIndex) => (
           <div
-            className={`flex flex-col gap-4 p-2 border-4 ${
+            className={`flex flex-col gap-6 md:gap-10 p-4 border-4 md:border-8 ${
               quizQuestionIndex % 2 == 0
                 ? "border-yellow-400"
                 : "border-blue-100"
             }`}
             key={quizQuestion.id}
           >
-            <p className="uppercase font-bold">
+            <p className="text-lg mt-4 md:text-2xl uppercase font-bold">
               Questao: {quizQuestionIndex + 1}
             </p>
 
-            <input
-              className="p-2 bg-gray-950 border-2 font-bold border-yellow-400"
-              type="text"
-              value={quizQuestion.question}
-              onChange={(event) =>
-                handleQuizQuestionTitleChange(event, quizQuestionIndex)
-              }
-            />
+            <div className="flex flex-col gap-4 md:w-2/3 md:m-auto">
+              <label className="text-lg md:text-2xl font-bold">Pergunta</label>
+              <input
+                className="text-lg md:text-2xl p-2 md:p-4 bg-gray-950 border-4 font-bold border-yellow-400"
+                type="text"
+                value={quizQuestion.question}
+                onChange={(event) =>
+                  handleQuizQuestionTitleChange(event, quizQuestionIndex)
+                }
+              />
+            </div>
             <button
-              className="flex items-center justify-center p-2 font-bold bg-red-500"
+              className="flex items-center justify-center rounded-md text-md md:text-3xl md:w-1/8 md:m-auto p-2 md:p-8 font-bold bg-red-500"
               onClick={() => removeQuizQuestion(quizQuestionIndex)}
             >
               <BsFillTrash3Fill />
@@ -170,7 +173,11 @@ export default function MakeQuiz(props: MakeQuizProps) {
             {quizQuestion.alternatives.map(
               (alternative, quizQuestionAlternativeIndex) => (
                 <div className="flex flex-col gap-4" key={alternative.id}>
-                  <div className={`flex justify-around items-center ${props.isMenuOpen ? "flex-col gap-y-3" : "flex-row"}`}>
+                  <div
+                    className={`flex justify-around items-center ${
+                      props.isMenuOpen ? "flex-col gap-y-3" : "flex-row"
+                    }`}
+                  >
                     <input
                       className="p-1 text-sm bg-gray-950 border-2 font-bold border-yellow-400"
                       type="text"
@@ -211,13 +218,13 @@ export default function MakeQuiz(props: MakeQuizProps) {
                 </div>
               )
             )}
-            <div className="w-full">
               <button
+                className="w-1/2 m-auto flex items-center gap-2 rounded-md bg-green-400 font-bold p-2 text-slate-700"
                 onClick={() => addQuizQuestionAlternative(quizQuestionIndex)}
               >
-                <BsPlusCircleFill size={30} />
+                <BsFillPlusSquareFill/>
+                Alternativa
               </button>
-            </div>
           </div>
         ))}
       </div>
